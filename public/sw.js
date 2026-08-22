5fbb676ae657d0b047b4f1aed308f28a84e4fcfb
MAYUR-17-08 <themayurjangid@gmail.com>
2026-08-22 13:50:43 +0530

    Added PWA Manifest & Service Worker for PWABuilder

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,7 @@
-const CACHE_NAME = 'fitube-v1';
-const STATIC_ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
-
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
-  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(self.clients.claim());
-});
-
 self.addEventListener('fetch', (e) => {
-  if (e.request.url.includes('/api/')) return;
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
